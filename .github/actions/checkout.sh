de4101ab478b62cc48a561bad7a946ab77a12c09
王飞 <wangfei@detadata.com>
2024-05-24 18:00:41 +0800

    修改

--- .github/actions/checkout.sh
+++
@@ -1,8 +0,0 @@
-#usr bin/env sh
-
-mkdir -p .github/actions/checkout
-cd .github/actions/checkout
-git init
-git remote add origin https://github.com/actions/checkout.git
-git fetch origin v4 --depth=1
-git checkout FETCH_HEAD
--- .github/actions/configure-pages.sh
+++
@@ -1,8 +0,0 @@
-#usr bin/env sh
-
-mkdir -p .github/actions/configure-pages
-cd .github/actions/configure-pages
-git init
-git remote add origin https://github.com/actions/configure-pages.git
-git fetch origin v4 --depth=1
-git checkout FETCH_HEAD
